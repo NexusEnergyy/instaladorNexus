4fb263e2ec647b57699799b6b783c7d272fa498b
Diogo Polastrine Duarte Silva <118745964+Polastrine@users.noreply.github.com>
2024-11-24 13:38:27 -0300

    Update instaladorNexus.sh

--- instaladorNexus.sh
+++ instaladorNexus.sh
@@ -16,13 +16,17 @@ sudo apt update && sudo apt upgrade -y
 echo "Atualização concluída..."
 clear
 
-
+echo "Verificando dependências e serviços necessários..."
 
 # Instalando o Git
-echo "Instalando o Git e clonando o repositório do projeto..."
-sudo apt install -y git || { echo "Erro ao instalar Git"; exit 1; }
-git clone https://github.com/NexusEnergyy/nexusEnergy-app.git || { echo "Erro ao clonar o repositório"; exit 1; }
-clear
+if ! git --version &> /dev/null; then
+  echo "Git não encontrado. Instalando Git..."
+  sudo apt update -y
+  sudo apt install -y git || { echo "Erro ao instalar Git"; exit 1; }
+  echo "Git instalado com sucesso."
+else
+  echo "Git já está instalado."
+fi
 
 # Verificando a instalação do Docker
 if ! command -v docker &> /dev/null
@@ -37,41 +41,23 @@ else
 fi
 
 # Verificando a instalação do Docker Compose
-if ! command -v docker-compose &> /dev/null && ! docker compose version &> /dev/null
-then
+if ! docker compose version &> /dev/null && ! docker-compose --version &> /dev/null; then
   echo "Docker Compose não encontrado. Instalando Docker Compose..."
+
+  # Atualiza pacotes e instala o plugin do docker-compose
+  sudo apt update -y
   sudo apt install -y docker-compose-plugin || { echo "Erro ao instalar Docker Compose"; exit 1; }
+
   echo "Docker Compose instalado com sucesso."
 else
   echo "Docker Compose já está instalado."
 fi
-clear
 
 
 # Construindo e iniciando containers com Docker Compose
 echo "Construindo e iniciando containers com Docker Compose..."
 cd nexusEnergy-app
 clear
-
-# Configurando credenciais temporárias da AWS
-echo "Insira suas credenciais AWS para configuração."
-read -p "AWS Access Key ID: " AWS_ACCESS_KEY_ID
-read -p "AWS Secret Access Key: " AWS_SECRET_ACCESS_KEY
-read -p "AWS Session Token: " AWS_SESSION_TOKEN
-clear
-
-# Salvando variáveis de ambiente localmente na máquina
-export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
-export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
-export AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN}
-
-# Salvando variáveis de ambiente no arquivo .env
-echo "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" >> .env
-echo "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" >> .env
-echo "AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN}" >> .env
-
-echo "Variáveis de ambiente da AWS configuradas no arquivo .env."
-
 
 sudo docker-compose up --build || { echo "Erro ao subir os containers"; exit 1; }
 
